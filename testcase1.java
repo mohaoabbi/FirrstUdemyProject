ce446f6a354789eb9ca8adf2fdba132855721145
MOHAMED ABBI <mohaoabbi@gmail.com>
2020-01-14 15:06:54 -0500

    adding one test case to the project

---
+++ testcase1.java
@@ -0,0 +1,15 @@
+
+public class testcase1 {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		if (hungry=="yes") {
+			System.out.println("Go ahead and eat somthing");
+			else
+				System.out.println("Keep working.....");
+		}
+
+	}
+
+}
